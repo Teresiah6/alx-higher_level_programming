5ed28e7157862615be420d0800558140ab55372b
Teresiah6 <wangariteresiah6@gmail.com>
2023-02-15 20:29:53 +0000

    mandatory

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,2 @@
+--lists all the cities of California in database hbtn_0d_usa
+SELECT cities FROM hbtn_0d_usa WHERE name ='California' ORDER BY cities.id;
---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,3 @@
+-- lists all cities in the database hbtn_0d_usa
+SELECT cities.id, cities.name, states.name FROM hbtn_0d_usa 
+ORDER BY cities.id;
