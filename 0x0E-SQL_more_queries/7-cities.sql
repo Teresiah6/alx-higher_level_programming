5d8197d139c10efa54f2c25f84b152d0ead3bc39
Teresiah6 <wangariteresiah6@gmail.com>
2023-06-15 15:13:19 +0300

    create db and table cities

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,12 @@
---creates the database hbtn_0d_usa and 
---the table cities (in the database hbtn_0d_usa) 
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities;
-DESCRIBE cities(id INT not null,
-state_id INT not null,name VARCHAR(256) not null);
+-- script creates db hbtn_0d_usa and the table cities
+-- create db 
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa; 
+-- use db
+USE hbtn_0d_usa 
+-- create table in db
+CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENT NOT NULL, 
+state_id INT NOT NULL, 
+name VARCHAR(255) NOT NULL, 
+PRIMARY KEY(id), 
+FOREIGN KEY(state_id) REFERENCES states(id));
+
