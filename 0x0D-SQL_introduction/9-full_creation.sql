de6c93a141aba52fb426b8f22b5b38127dce0353
Teresiah6 <wangariteresiah6@gmail.com>
2023-06-15 13:20:20 +0300

    create second table in db

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,7 @@
---create table second_table in db hbtn_0c_0
-CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
---add multiple rows id INT, nameVARCHAR(256) and score INT
+-- creates second_table in database hbtn_0c_0 and add multiples rows
+-- creates a new table
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+-- adds new rows to table
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, 'John', 10);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, 'Alex', 3);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, 'Bob', 14);
