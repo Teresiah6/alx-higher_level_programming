ab203dbdcf7211a2aef20d8c067ab4c0e8f3dbfe
Teresiah6 <wangariteresiah6@gmail.com>
2023-06-15 14:33:48 +0300

    list all privileges of mysql users

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,5 @@
--- list all privileges of users user_0d_1 and user_0d_2
-SHOW GRANT FOR user_0d_1@localhost AND user_0d_2@localhost;
+-- lists all privileges of user_0d_1 and user_0d_2
+-- lists all privileged for user 1
+SHOW GRANTS FOR user_0d_1@localhost;
+-- lists all privileged for user
+SHOW GRANTS FOR user_0d_2@localhost;
