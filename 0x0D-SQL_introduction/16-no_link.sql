dafa39d4e1b62e6dd2bb3506ed63cb9a94fceb31
Teresiah6 <wangariteresiah6@gmail.com>
2023-06-15 13:53:03 +0300

    list all records of second table of db

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,3 @@
--- list records of second_table 
+-- list records of second_table
 --list row with name value and score and name ordered
-SELECT score, name
-FROM second_table
-WHERE name IS NOT NULL
-ORDER BY score DESC;
+SELECT `score`, `name` FROM second_table WHERE `name` IS NOT NULL ORDER BY `score` DESC;
