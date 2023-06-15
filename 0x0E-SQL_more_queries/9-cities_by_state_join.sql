b032e0616059245d23b2f25e068201ebe62b2bd7
Teresiah6 <wangariteresiah6@gmail.com>
2023-06-15 15:21:43 +0300

    list all cities contained in db

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,2 @@
 -- lists all cities in the database hbtn_0d_usa
-SELECT cities.id, cities.name, states.name FROM hbtn_0d_usa 
-ORDER BY cities.id;
+SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON states.id =  cities.state_id ORDER BY cities.id;
