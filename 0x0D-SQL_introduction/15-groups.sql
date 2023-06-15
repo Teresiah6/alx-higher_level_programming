9b44daa0e993de53fb7e6dff3141d93bd1349837
Teresiah6 <wangariteresiah6@gmail.com>
2023-06-15 13:48:48 +0300

    list number of records of same score in second_table

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,2 @@
---list the number of records of same score in second_table
-SELECT score, COUNT(*) AS number
-FROM second_table
-GROUP BY score
-ORDER BY number DESC;
+--script lists the number of records of same score in second_table
+SELECT `score`, COUNT(`score`) 'number' FROM 'second_table' GROUP BY `score` ORDER BY `number` DESC;
